6862584a87462cfcc753ac05037cbc064857829c
nozomikobori <nozomikobori@Nozomis-MacBook-Pro.local>
2021-02-23 21:27:28 +0900

    Add Section 6

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
+/* eslint-disable no-useless-constructor */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SeasonDisplay from './SeasonDisplay';
+
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     );
+
+//     return <div>Latitude: </div>
+// };
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: '' }; 
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude });
+            },
+            err => {
+                this.setState({errorMessage: err.message});
+            }
+        );
+    }
+
+    // React says we have to define render!!
+    render() {
+        // return (
+        //     <div>
+        //         Latitude: {this.state.lat}
+        //         <br/>
+        //         Error: {this.state.errorMessage}
+        //     </div>
+        // )
+        if(this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            )
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return (
+                <div>
+                    Latitude: {this.state.lat}
+                </div>
+            )
+        }
+        return (
+            <div>Loading...</div>
+        )
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, 
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
