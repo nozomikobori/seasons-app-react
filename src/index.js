014efcd2f821e633108acbbbe866e431ecd6f58f
nozomikobori <nozomikobori@Nozomis-MacBook-Pro.local>
2021-02-23 23:21:07 +0900

    Add Section 6

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-useless-constructor */
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import "semantic-ui-css/semantic.min.css";
 import SeasonDisplay from './SeasonDisplay';
-
+import Spinner from './Spinner';
 // const App = () => {
 //     window.navigator.geolocation.getCurrentPosition(
 //         position => console.log(position),
@@ -13,46 +14,46 @@ import SeasonDisplay from './SeasonDisplay';
 // };
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    // constructor(props) {
+    //     super(props);
 
-        this.state = { lat: null, errorMessage: '' }; 
+    //     this.state = { lat: null, errorMessage: '' };    
+    // }
 
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat: position.coords.latitude });
-            },
-            err => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({lat: position.coords.latitude }),
+            err => this.setState({errorMessage: err.message})
         );
+    }
+
+    // componentDidMount() {
+    //     console.log('My component was rendered to the screen');
+    // }
+
+    // componentDidUpdate() {
+    //     console.log('My component was just updated - it rendered.');
+    // }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div> Error: {this.state.errorMessage} </div>;
+    
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        return <Spinner message="Please accept location request" />;
     }
 
     // React says we have to define render!!
     render() {
-        // return (
-        //     <div>
-        //         Latitude: {this.state.lat}
-        //         <br/>
-        //         Error: {this.state.errorMessage}
-        //     </div>
-        // )
-        if(this.state.errorMessage && !this.state.lat) {
-            return (
-                <div>
-                    Error: {this.state.errorMessage}
-                </div>
-            )
-        }
-        if(!this.state.errorMessage && this.state.lat) {
-            return (
-                <div>
-                    Latitude: {this.state.lat}
-                </div>
-            )
-        }
         return (
-            <div>Loading...</div>
+            <div className="border red">
+                {this.renderContent()}
+            </div>
         )
     }
 }
